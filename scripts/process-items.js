4b66ec1d4dc8bc80e5df098235decc6ee41fe7da
chris <2020386+codeguy1134@users.noreply.github.com>
2026-05-12 01:09:03 -0400

    Finished process-items.js to parse & clean pf2e/equipment and create items.json.

--- scripts/process-items.js
+++ scripts/process-items.js
@@ -7,12 +7,29 @@ const outputFile = path.join(__dirname, '../data/items.json');
 const files = fs.readdirSync(sourceFolder)
   .filter(f => f.endsWith('.json'));
 
-// console.log(`Found ${files.length} files`);
 
+// creates an array of files found in ../packs/pf2e/equipment
 const items = files.map(f => {
     const raw = fs.readFileSync(path.join(sourceFolder, f), 'utf8');
     return JSON.parse(raw);
 });
 
-console.log(`Loaded ${items.length} items`);
-console.log('First item name:', items[0].name);
+
+// creates a new .json file of those items with only the descriptors we need
+const cleaned = items
+    .filter(item => item.system?.level?.value !== undefined)
+    .map(item => ({
+        name: item.name,
+        img: item.img,
+        type: item.type,
+        category: item.system.category,
+        level: item.system.level.value,
+        price: item.system.price.value,
+        bulk: item.system.bulk.value,
+        rarity: item.system.traits.rarity,
+        traits: item.system.traits.value,
+        source: item.system.publication.title,
+        description: item.system.description.value
+    }));
+
+    fs.writeFileSync(outputFile, JSON.stringify(cleaned, null, 2));
